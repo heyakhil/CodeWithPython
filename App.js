437ef2507d920c3a1ee4dcbb38ae0fb3ba0deb78
farhan <farhanalam38@gmail.com>
2020-06-15 12:56:01 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
-//The Color combination of the app is #7b02de and #fff
-import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React from 'react'
+import {View,Text,StyleSheet,Button,TouchableOpacity,Image } from "react-native";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Splash from "./Splash";
+import Register from "./Register";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello Farhan Design the Login Signup Here</Text>
-        <Text>Ok, i'm ready to design Login Signup page Here.</Text>
-      </View>
-    )
-  }
+
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+    render(){
+        return(
+          
+    <NavigationContainer>
+    <Stack.Navigator  
+        screenOptions={{
+          headerShown: false
+        }}
+    >
+      <Stack.Screen name="Splash" component={Splash} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
+    </Stack.Navigator>
+    </NavigationContainer>
+    
+        )
+    }
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+
+
